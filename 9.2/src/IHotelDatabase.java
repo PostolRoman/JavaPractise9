856494ad6a8a9a34a6e7ff33f476227a21fc5428
PostolRoman <postolromanv@gmail.com>
2025-02-05 16:36:11 +0100

    Add files via upload

---
+++ 9.2/src/IHotelDatabase.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+interface IHotelDatabase {
+    List<String> getAvailableRooms();
+    void bookRoom(String roomNumber);
+    void cancelBooking(String roomNumber);
+    void checkIn(String roomNumber);
+    void checkOut(String roomNumber);
+}
